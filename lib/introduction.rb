1dfe43c99da6e5bd2b22f920837f9827fe90545a
Steve Anderson <prototype909@msn.com>
2020-05-07 19:52:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,11 +2,11 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-introduction("Josh")
+introduction("Steve")
 
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language("Dan", "Ember.js")
+introduction_with_language("Steve", "Ruby")
